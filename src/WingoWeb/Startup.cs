6c5e24bb290f00611c76d5013eff1b3b0230e842
Wingo <cmwingo@gmail.com>
2017-03-03 12:47:14 -0800

    Add bower, bootstrap, jquery, and navbar

--- src/WingoWeb/Startup.cs
+++ src/WingoWeb/Startup.cs
@@ -8,13 +8,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using WingoWeb.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace WingoWeb
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
@@ -24,6 +23,8 @@ namespace WingoWeb
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole();
+
+            app.UseStaticFiles();
 
             if (env.IsDevelopment())
             {
