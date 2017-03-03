5de2867b9c4b194364989942ff2a4b868128c73f
Wingo <cmwingo@gmail.com>
2017-03-03 10:25:26 -0800

    Get good responses back from Github API

---
+++ src/WingoWeb/Models/Repo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
+using WingoWeb.Models;
+
+namespace WingoWeb.Models
+{
+    public class Repo
+    {
+        public string Owner { get; set; }
+        public string Name { get; set; }
+        public static List<Repo> GetRepos()
+        {
+            var client = new RestClient("https://api.github.com/users/" + EnvironmentVariables.userName + "/repos");
+            client.Authenticator = new HttpBasicAuthenticator(EnvironmentVariables.userName, EnvironmentVariables.password);
+            var request = new RestRequest();
+            request.AddHeader("User-Agent", EnvironmentVariables.userName);
+
+            var response = new RestResponse();
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            var repoList = JsonConvert.DeserializeObject<List<Repo>>(jsonResponse.ToString());
+            return repoList;
+        }
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
--- src/WingoWeb/Startup.cs
+++ src/WingoWeb/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using WingoWeb.Models;
 
 namespace WingoWeb
 {
@@ -16,6 +17,7 @@ namespace WingoWeb
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +29,13 @@ namespace WingoWeb
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
 
             app.Run(async (context) =>
             {
